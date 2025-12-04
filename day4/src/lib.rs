528c3e5f1362488fb36ea18802675dcf9c6681f3
Pietro Di Bello <pierodibello@gmail.com>
2025-12-04 22:23:40 +0100

    feat: count all paper rolls (not yet checking neighbors)

--- day4/src/lib.rs
+++ day4/src/lib.rs
@@ -1,5 +1,5 @@
-pub fn count_accessible_rolls(_grid: &str) -> usize {
-    1
+pub fn count_accessible_rolls(grid: &str) -> usize {
+    grid.chars().filter(|&c| c == '@').count()
 }
 
 #[cfg(test)]
@@ -10,5 +10,11 @@ mod tests {
     fn single_paper_roll_with_no_neighbors_is_accessible() {
         let grid = "@";
         assert_eq!(count_accessible_rolls(grid), 1);
+    }
+
+    #[test]
+    fn empty_grid_has_no_accessible_rolls() {
+        let grid = ".";
+        assert_eq!(count_accessible_rolls(grid), 0);
     }
 }
