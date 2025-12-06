530ce2bc4bf27cfceb5e37edc5ea8687cd91abc6
Pietro Di Bello <pierodibello@gmail.com>
2025-12-06 12:02:34 +0100

    feat(day6): add main.rs to run solution with puzzle input

---
+++ day6/src/main.rs
@@ -0,0 +1,8 @@
+use day6::solve;
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("puzzle-input.txt").expect("Failed to read input file");
+    let result = solve(&input);
+    println!("Part 1 Answer: {}", result);
+}
