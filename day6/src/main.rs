7f6c9975d6ddc1de3a3922f0f97cb2405e86f1c7
Pietro Di Bello <pierodibello@gmail.com>
2025-12-06 12:07:25 +0100

    feat(day6): update main.rs to run Part Two solution

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,8 +1,10 @@
-use day6::solve;
+use day6::{solve, solve_part2};
 use std::fs;
 
 fn main() {
     let input = fs::read_to_string("puzzle-input.txt").expect("Failed to read input file");
     let result = solve(&input);
     println!("Part 1 Answer: {}", result);
+    let result_part2 = solve_part2(&input);
+    println!("Part 2 Answer: {}", result_part2);
 }
