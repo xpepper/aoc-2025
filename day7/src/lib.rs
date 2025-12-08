e6502991ef5d50b91090dd66727723bbf71f5c03
Pietro Di Bello <pierodibello@gmail.com>
2025-12-08 18:09:12 +0100

    feat: implement parsing to find start position

--- day7/src/lib.rs
+++ day7/src/lib.rs
@@ -1,5 +1,27 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Debug, PartialEq)]
+pub struct Point {
+    pub x: usize,
+    pub y: usize,
+}
+
+pub struct Grid {
+    pub start: Point,
+}
+
+pub fn parse(input: &str) -> Grid {
+    let mut start = Point { x: 0, y: 0 };
+    for (y, line) in input.lines().enumerate() {
+        for (x, char) in line.chars().enumerate() {
+            if char == 'S' {
+                start = Point { x, y };
+            }
+        }
+    }
+    Grid { start }
+}
+
+pub fn solve(_input: &str) -> u64 {
+    0
 }
 
 #[cfg(test)]
@@ -7,8 +29,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn parse_finds_start_position() {
+        let input = "..\n.S";
+        let grid = parse(input);
+        assert_eq!(grid.start, Point { x: 1, y: 1 });
     }
 }
